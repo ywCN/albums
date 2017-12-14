eda59f2e0fca5703db8d7e5866be2aa5f9234acc
ywCN <wangyouhao90@gmail.com>
2017-12-14 17:53:22 -0500

    added style

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,31 @@ import React from 'react';
 import { View } from 'react-native';
 
 // Create this new Component instead of using View
-// because of reusing styles
+// because of reusing styles (no CSS file)
 
 const Card = () => {
     return (
-        <View></View>
+        <View style={styles.containerStyle}></View>
     );
+};
+
+const styles = {
+    containerStyle: {
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10
+    }
 };
 
 export default Card;
