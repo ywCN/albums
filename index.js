4c60c27abcc1de3fe845dbe85d333d15eb7cc14d
ywCN <wangyouhao90@gmail.com>
2017-12-14 15:02:32 -0500

    tested AlbumList

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // create a component
 const App = () => (
     // <Header></Header>
-    <Header headerText={'Albums'} />
+    <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
 );
 
 // Render it to the device
