7601fbecbf9b440977f88f1adc0d71667f4f7402
ywCN <wangyouhao90@gmail.com>
2017-12-14 15:50:58 -0500

    tried debugger

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+    // this method will be called automatically
+    componentWillMount() {
+        console.log('componentWillMount in AlbumList is called!!!!');
+        // debugger;
+    }
+
     render() {
         return (
             <View>
