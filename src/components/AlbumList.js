dafb29afd9bd7d3259df8a096c71db4c3f5b471c
ywCN <wangyouhao90@gmail.com>
2017-12-14 16:52:20 -0500

    used state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,18 +3,24 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+    // state property of component
+    state = { alnums: [] };
+
     // this lifecycle method will be called automatically
     // like init()
     componentWillMount() {
         // this call is async, so we need to know when it is completed
         // then() is for things after the AJAX request is completed
-        axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => console.log(response));
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => this.setState({ albums: response.data }));
         
         // console.log('componentWillMount in AlbumList is called!!!!');
         // debugger;
     }
 
     render() {
+        console.log(this.state); // will be called twice
+
         return (
             <View>
                 <Text>Album List!!!</Text>
