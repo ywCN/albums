9916c31cdba618369d224b182ec079c2dcd189eb
ywCN <wangyouhao90@gmail.com>
2017-12-14 10:51:34 -0500

    created and used style

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,16 @@ import React from 'react';
 import { Text } from 'react-native';
 
 // make a component
-const Header = () => (
-    <Text>Albums!</Text>
-);
+const Header = () => {
+    const { textStyle } = styles;
+    return <Text style={textStyle}>Albums!</Text>;
+};
+
+const styles = {
+    textStyle: {
+        fontSize: 50
+    }
+};
 
 // make the component available to other parts of the app
 export default Header;
