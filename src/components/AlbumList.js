24cb3add7bf15de7a0b48ffaafd8bf56e584b5ac
ywCN <wangyouhao90@gmail.com>
2017-12-14 16:04:17 -0500

    more comment

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,10 +3,13 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
-    // this method will be called automatically
+    // this lifecycle method will be called automatically
     // like init()
     componentWillMount() {
+        // this call is async, so we need to know when it is completed
+        // then() is for things after the AJAX request is completed
         axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => console.log(response));
+        
         // console.log('componentWillMount in AlbumList is called!!!!');
         // debugger;
     }
