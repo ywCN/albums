d77068bd17a6d39b901abf405028a632ddcf54c2
ywCN <wangyouhao90@gmail.com>
2017-12-14 10:36:31 -0500

    finish this component

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,11 @@
 // import libraries for making a component
-
+import React from 'react';
+import { Text } from 'react-native';
 
 // make a component
-
+const Header = () => (
+    <Text>Albums!</Text>
+);
 
 // make the component available to other parts of the app
+export default Header;
