e16423b7f71ca6e1101798ca9d09317936c59834
ywCN <wangyouhao90@gmail.com>
2017-12-14 14:56:09 -0500

    added a new component

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // import a library to help create a component
 import React from 'react';
 import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
 
 // create a component
 const App = () => (
