fe46b6e4faa7baeee14477395f82c1b2f99f82e0
ywCN <wangyouhao90@gmail.com>
2017-12-14 10:59:44 -0500

    added and used style

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,23 @@
 // import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // make a component
 const Header = () => {
-    const { textStyle } = styles;
-    return <Text style={textStyle}>Albums!</Text>;
+    const { textStyle, viewStyle } = styles;
+    return (
+        <View style={viewStyle}>
+        <Text style={textStyle}>Albums!</Text>
+        </View>
+    );
 };
 
 const styles = {
     textStyle: {
-        fontSize: 50
+        fontSize: 20
+    },
+    viewStyle: {
+        backgroundColor: '#F8F8F8'
     }
 };
 
