e311f57318a2811f7f480baa909970167ce36d1d
ywCN <wangyouhao90@gmail.com>
2017-12-14 17:46:09 -0500

    comment

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { View } from 'react-native';
 
+// Create this new Component instead of using View
+// because of reusing styles
+
 const Card = () => {
     return (
         <View></View>
