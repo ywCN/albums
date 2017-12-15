dc0c4c8d551a80fd0c671a8a72cf6c3048c425e7
ywCN <wangyouhao90@gmail.com>
2017-12-14 19:22:44 -0500

    added pic

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -8,12 +8,15 @@ import CardSection from './CardSection';
 // so this component can be a functional component
 
 const AlbumDetail = ({ album }) => {
-    const {title, artist, thumnail_image } = album;
+    const { title, artist, thumnail_image } = album;
     return (
         <Card>
             <CardSection>
                 <View>
-                    <Image source={{ uri: thumnail_image }} />
+                    <Image 
+                        style={styles.thumnailStyle}
+                        source={{ uri: thumnail_image }} 
+                    />
                 </View>
                 <View style={styles.headerContentStyle}>
                     <Text>{title}</Text>
@@ -28,6 +31,11 @@ const styles = {
     headerContentStyle: {
         flexDirection: 'column',
         justifyContent: 'space-around'
+    },
+
+    thumnailStyle: {
+        height: 50,
+        width: 50
     }
 };
 
