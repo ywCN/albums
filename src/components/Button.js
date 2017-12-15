9ffa775c81c4425c44cbc264cd8f898e52f1af1a
ywCN <wangyouhao90@gmail.com>
2017-12-14 20:20:27 -0500

    added touchable button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
+
+//TouchableOpacity is just a button
 
 const Button = () => {
     return (
-        <Text>Click me!!!</Text>
+        <TouchableOpacity>
+            <Text>Click me!!!</Text>
+        </TouchableOpacity>
     );
 };
 
