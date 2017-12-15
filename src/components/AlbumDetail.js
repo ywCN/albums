69319f467ec8762925ce4c5711bd34e2b2070040
ywCN <wangyouhao90@gmail.com>
2017-12-14 19:28:46 -0500

    added style for view

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -9,11 +9,15 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
     const { title, artist, thumbnail_image } = album;
-    const { thumbnailStyle, headerContentStyle } = styles;
+    const { 
+        thumbnailStyle,
+        headerContentStyle,
+        thumbnailContainerStyle
+    } = styles;
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image 
                         style={thumbnailStyle}
                         source={{ uri: thumbnail_image }} 
@@ -37,6 +41,13 @@ const styles = {
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
     }
 };
 
